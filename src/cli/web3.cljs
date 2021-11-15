73f330bb384c41a0d578ad487d069675ac1f8251
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-11-14 18:14:09 -0800

    refactor(cli): separate aggregator

--- src/cli/web3.cljs
+++ src/cli/web3.cljs
@@ -1,56 +1,9 @@
 (ns cli.web3
   (:require
-    [cljs.core :as cljs]
     ["web3" :as web3js]
     ["eth-provider" :as eth-provider]
-    [cli.utils :refer [display-price]]))
-
-
-(def aggregatorV3InterfaceABI
-  (cljs/clj->js
-    [{"inputs" []
-      "name" "decimals"
-      "outputs" [{ "internalType" "uint8" "name" "" "type" "uint8"}]
-      "stateMutability" "view"
-      "type" "function"}
-     {"inputs" []
-      "name" "description"
-      "outputs" [{ "internalType" "string" "name" "" "type" "string"}]
-      "stateMutability" "view"
-      "type" "function"}
-     {"inputs" [{"internalType" "uint80" "name" "_roundId" "type" "uint80"}]
-      "name" "getRoundData"
-      "outputs" [{"internalType" "uint80" "name" "roundId" "type" "uint80"}
-                 { "internalType" "int256" "name" "answer" "type" "int256"}
-                 { "internalType" "uint256" "name" "startedAt" "type" "uint256"}
-                 { "internalType" "uint256" "name" "updatedAt" "type" "uint256"}
-                 { "internalType" "uint80" "name" "answeredInRound" "type" "uint80"}]
-      "stateMutability" "view"
-      "type" "function"}
-     {"inputs" []
-      "name" "latestRoundData"
-      "outputs" [{"internalType" "uint80"
-                  "name" "roundId"
-                  "type" "uint80"}
-                 {"internalType" "int256"
-                  "name" "answer"
-                  "type" "int256"}
-                 {"internalType" "uint256"
-                  "name" "startedAt"
-                  "type" "uint256"}
-                 {"internalType" "uint256"
-                  "name" "updatedAt"
-                  "type" "uint256"}
-                 {"internalType" "uint80"
-                  "name" "answeredInRound"
-                  "type" "uint80"}]
-      "stateMutability" "view"
-      "type" "function"}
-     {"inputs" []
-      "name" "version"
-      "outputs" [{ "internalType" "uint256" "name" "" "type" "uint256"}]
-      "stateMutability" "view"
-      "type" "function"}]))
+    [cli.utils :refer [display-price]]
+    [cli.chain-link :refer [aggregatorV3InterfaceABI]]))
 
 (defn create
   "Create a web3 instance"
