44d9686d0f854f9fd8e1bf1d2214d56f1ff439d0
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-11-14 21:47:01 -0800

    fix(cli): force exit
    not sure why it hangs but web3 isn't closing

--- src/cli/core.cljs
+++ src/cli/core.cljs
@@ -4,19 +4,22 @@
     ["yargs" :as yargs]
     [cli.web3 :as web3]))
 
+
 (defn handle-token-agg [token]
   (->
     (web3/create)
     (web3/get-token-usd token)
     (.then print)
-    (.then web3/close)))
+    (.then web3/close)
+    (.then js/process.exit)))
 
 (defn handle-gas-price []
   (->
     (web3/create)
     (web3/get-gas)
     (.then print)
-    (.then web3/close)))
+    (.then web3/close)
+    (.then js/process.exit)))
 
 (defn get-args
   ([] (get-args nil))
@@ -36,20 +39,26 @@
                {:default "eth"
                 :defaultDescription "Look up the current aggregate price of Ethereum to fiat USD"
                 :description "token to look up"
-                :type "string"}))))
-       #(handle-token-agg (:token (cljs/js->clj %1 :keywordize-keys true))))
+                :type "string"})))))
      (.command
        "gas"
        "get the current Ethereum fast-gas price in GWei"
-       (cljs/clj->js {})
-       handle-gas-price)
+       (cljs/clj->js {}))
      (.parse (or ex js/undefined))
      (cljs/js->clj :keywordize-keys true))))
 
 (comment
+  (get-args "eth")
   (get-args "btc")
   (get-args "agg")
   (get-args "gas"))
 
 (defn ^:export main []
-  (get-args))
+  (let [argv (get-args)
+        token (:token argv)]
+    (if
+      token (handle-token-agg token)
+      (handle-gas-price))))
+
+(comment
+  (main))
