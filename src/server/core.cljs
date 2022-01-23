70b09a803eba1efe595b468b36ce2ac466d3e9a2
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-01-22 22:17:02 -0800

    feat(server): init pathom3

--- src/server/core.cljs
+++ src/server/core.cljs
@@ -2,11 +2,32 @@
   (:require
     [taoensso.timbre :as timbre :refer-macros [info]]
     [integrant.core :as ig]
+
+    [com.wsscode.pathom3.connect.operation :as pco :refer-macros [defresolver]]
+    [com.wsscode.pathom3.connect.indexes :as pci]
+    [com.wsscode.pathom3.interface.eql :as peql]
+    [com.wsscode.pathom.viz.ws-connector.core :as pvc]
+    [com.wsscode.pathom.viz.ws-connector.pathom3 :as p.connector]
+
     [server.infra.core]
     [server.routes]
     [server.system]
     [server.config :refer [env]]
     [server.services.coingecko.core]))
+
+
+(defresolver coin-price [{:coin/keys [sym]}]
+  {::pco/output [:coin/price]}
+  (info sym)
+  {:coin/price 91.38})
+
+(def pathom-env
+  (->
+    (pci/register [coin-price])
+    (p.connector/connect-env {::pvc/parser-id `pathom-env})))
+
+(comment (peql/process pathom-env [{[:coin/sym "btc"] [:coin/price]}]))
+
 
 (defonce system-ref (atom nil))
 
